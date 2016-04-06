edfb1cbb97892ac8de7223d03d06fe97aa25f836
Hank Ehly <hank.ehly@mulodo.com>
2016-04-06 16:27:12 +0900

    Remove description

--- angular2_startup.sh
+++ angular2_startup.sh
@@ -9,11 +9,13 @@ main() {
   git clone https://github.com/hehly/angular2-starter.git
   cd angular2-starter || exit 1
 
-  read -p "name (no spaces..): (${__dirname}) " _app_name
-  read -p "description: (\"\") " _app_description
+  read -p "name (spaces will cause crash) : (${__dirname}) " _app_name
+#  read -p "description: (\"\") " _app_description
 
   if [ -z ${_app_name} ]; then _app_name=${__dirname}; fi
-  if [ -z ${_app_description} ]; then _app_description=""; fi
+#  if [ -z ${_app_description} ]; then _app_description=""; fi
+
+  _app_description="";
 
   sed -i '' "s|{APP_NAME}|${_app_name}|" "package.json"
   sed -i '' "s|{APP_DESC}|${_app_description}|" "package.json"
